f82068b0b000a1a78e3596c15ddd673034828a34
neozhuo <13249625944@163.com>
2020-06-14 12:37:47 +0800

    first commit

--- vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
+++ vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/VhrApplication.java
@@ -1,5 +1,6 @@
 package org.javaboy.vhr;
 
+
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,9 +8,6 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
-@MapperScan(basePackages = "org.javaboy.vhr.mapper")
-@EnableScheduling
 public class VhrApplication {
 
     public static void main(String[] args) {
