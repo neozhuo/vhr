f742b6c8169ecf45ccf14c584ea625a066709de2
neozhuo <13249625944@163.com>
2020-06-18 20:57:16 +0800

    12 系统管理中 职称管理功能

--- vhr/vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/JobService.java
+++ vhr/vhrserver/vhr-service/src/main/java/org/javaboy/vhr/service/JobService.java
@@ -5,6 +5,7 @@ import org.javaboy.vhr.model.JobLevel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -15,5 +16,23 @@ public class JobService {
 
     public List<JobLevel> getAllJobLevel(){
         return jobLevelMapper.getAllJobLevels();
+    }
+
+    public Integer addJobLevel(JobLevel jobLevel){
+        jobLevel.setEnabled(true);
+        jobLevel.setCreateDate(new Date());
+        return jobLevelMapper.insertSelective(jobLevel);
+    }
+
+    public Integer updateJobLevel(JobLevel jobLevel){
+        return jobLevelMapper.updateByPrimaryKeySelective(jobLevel);
+    }
+
+    public Integer deleteJobLevel(Integer id){
+        return jobLevelMapper.deleteByPrimaryKey(id);
+    }
+
+    public Integer deleteJobLevels(Integer[] ids){
+        return jobLevelMapper.deleteJobLevelsByIds(ids);
     }
 }
--- vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/controller/JobLevelController.java
+++ vhr/vhrserver/vhr-web/src/main/java/org/javaboy/vhr/controller/JobLevelController.java
@@ -1,11 +1,10 @@
 package org.javaboy.vhr.controller;
 
 import org.javaboy.vhr.model.JobLevel;
+import org.javaboy.vhr.model.RespBean;
 import org.javaboy.vhr.service.JobService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,4 +19,38 @@ public class JobLevelController {
     public List<JobLevel> getAllJobLevels(){
         return jobService.getAllJobLevel();
     }
+
+    @PostMapping("/")
+    public RespBean addJobLevel(@RequestBody JobLevel jobLevel){
+        if(jobService.addJobLevel(jobLevel)==1){
+            return RespBean.ok("添加职称成功");
+        }
+        return RespBean.error("添加职称失败");
+    }
+
+    @PutMapping("/")
+    public RespBean updateJobLevel(@RequestBody JobLevel jobLevel){
+        if(jobService.updateJobLevel(jobLevel)==1){
+            return RespBean.ok("更新职称成功");
+        }
+        return RespBean.error("更新职称失败");
+    }
+
+    @DeleteMapping("/{id}")
+    public RespBean deleteJobLevelById(@PathVariable Integer id){
+        if(jobService.deleteJobLevel(id)==1){
+            return RespBean.ok("删除成功");
+        }
+        return RespBean.error("删除失败");
+    }
+
+    @DeleteMapping("/")
+    public RespBean deleteJobLevelByIds(Integer[] ids){
+        if(jobService.deleteJobLevels(ids)==ids.length){
+            return RespBean.ok("批量删除成功");
+        }
+        return RespBean.error("批量删除失败");
+    }
+
+
 }
